7efa2ba491e50630aae275521e1bd956508d5bec
Amanda Quaranto <amandaquaranto@AmandaMac.local>
2012-10-02 15:30:10 -0400

    Completed the initalize method and have next making the string required to pass most of the tests.

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -1,15 +1,34 @@
 class LookAndSay
   
   def initialize(string)
-    # Implement me!
+  	#if the input is an array join it into a string 
+  	#raise ArgumentError, "Please use only digits" if string.match(/[A-Za-z]+/) 
+  	@string = string
+    if @string.is_a? Array
+  		@working_string = string.join 
+  	elsif @string.is_a? String 
+  		@working_string = string
+    else
+      raise ArgumentError, "Please use only digits" 
+  	end
+  	#and check if it's digits, if it's letters raise an Argument Error
+  	raise ArgumentError, "Please use only digits" unless @working_string.match(/[0-9]+/) 
+	#break up the digits into groups of like numbers
+    @raw_str = @working_string.scan(/((\d)\2*)/).map(&:first)
+    @raw_str
   end
 
   def next
-    # Implement me!
+    grp = ""
+    @raw_str.each do |newgrp|
+     	grp << newgrp.length.to_s + newgrp[0]
+    end
+    grp
   end
 
-  def to_s
+  #def to_s
     # Implement me!
-  end
-  
+  #end
+
 end
+

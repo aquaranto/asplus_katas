e02dd152855d1aecf7b9ef560fa07633eec41fec
Amanda Quaranto <amandaquaranto@AmandaMac.local>
2012-10-08 11:13:35 -0400

    completed look and say

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -3,6 +3,8 @@ class LookAndSay
   def initialize(string)
   	#if the input is an array join it into a string 
   	#raise ArgumentError, "Please use only digits" if string.match(/[A-Za-z]+/) 
+    #and check if it's digits, if it's letters raise an Argument Error
+    #break up the digits into groups of like numbers
   	@string = string
     if @string.is_a? Array
   		@working_string = string.join 
@@ -11,24 +13,28 @@ class LookAndSay
     else
       raise ArgumentError, "Please use only digits" 
   	end
-  	#and check if it's digits, if it's letters raise an Argument Error
-  	raise ArgumentError, "Please use only digits" unless @working_string.match(/[0-9]+/) 
-	#break up the digits into groups of like numbers
-    @raw_str = @working_string.scan(/((\d)\2*)/).map(&:first)
-    @raw_str
+  	
+    raise ArgumentError, "Please use only digits" unless @working_string.match(/[0-9]+/) 
+
+    @solution = parse_string @working_string
   end
 
   def next
-    grp = ""
-    @raw_str.each do |newgrp|
-     	grp << newgrp.length.to_s + newgrp[0]
+    interim = ""
+    @solution.each do |newgrp|
+    	interim << newgrp.length.to_s + newgrp[0]
     end
-    grp
+    @solution = parse_string interim
+    self
   end
 
-  #def to_s
-    # Implement me!
-  #end
+  def parse_string(string)
+    string.scan(/((\d)\2*)/).map(&:first)
+  end
+
+  def to_s
+     @solution.join
+  end
 
 end
 
